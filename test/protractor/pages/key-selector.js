7c7d12d2181d97062e9efc1cefa49c6009365856
Matthew Collier <collier.matthew@gmail.com>
2016-07-30 18:35:27 -0400

    Revise protractor pages.

--- test/protractor/pages/key-selector.js
+++ test/protractor/pages/key-selector.js
@@ -8,6 +8,7 @@ var element = global.element;
 var should = global.should;
 var expect = global.expect;
 var protractor = global.protractor;
+var EC = protractor.ExpectedConditions;
 
 api.get = function(slug) {
   bedrock.get('/i/' + slug);
@@ -15,18 +16,23 @@ api.get = function(slug) {
 };
 
 api.addKeyFromModal = function(name) {
-  var modal = element(by.modal());
-  modal.element(by.partialButtonText('Add Key')).click();
-  bedrock.waitForModalTransition();
+  var modal = element(by.tagName('br-modal'));
+  browser.wait(EC.visibilityOf(modal), 3000);
+  modal.element(by.buttonText('Add Key')).click();
+  browser.wait(
+    EC.visibilityOf(element(by.tagName('br-generate-key-pair-modal'))), 3000);
   element(by.partialButtonText('Generate Key')).click();
-  bedrock.waitForModalTransition();
-  if(name !== undefined) {
+  // extend timeout, key generation can take some time
+  browser.wait(
+    EC.elementToBeClickable(element(by.buttonText('Save'))), 30000);
+  if(name) {
     element(by.model('$ctrl.model.key.label'))
       .clear()
       .sendKeys(name);
   }
-  element(by.partialButtonText('Save')).click();
-  bedrock.waitForModalTransition();
+  element(by.buttonText('Save')).click();
+  browser.wait(
+    EC.invisibilityOf(element(by.tagName('br-generate-key-pair-modal'))), 3000);
 };
 
 api.deselectKeyFromModal = function() {
